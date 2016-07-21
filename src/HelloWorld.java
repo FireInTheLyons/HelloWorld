bf8cf57c908621368c8cad1a1caaff3b2822c8d6
John Lyons <mrlyoneye@gmail.com>
2016-07-21 13:07:23 +0100

    another test. Testing to see if this will update properly.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,10 @@
-/**
- * Created by t00158576 on 21/07/2016.
- */
 public class HelloWorld {
 
     public static void main (String args[]) {
 
-        System.out.print("Hello World!");
+        System.out.print("Hello GitHub!");
+        System.out.print("This is the second test!");
+
 
     }
 
